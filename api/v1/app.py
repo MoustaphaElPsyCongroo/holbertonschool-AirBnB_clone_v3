1076d568337efba9d5a3d741931b7c2af07f4d3e
MousElPsyCongroo <mtapha.tall@gmail.com>
2022-12-06 15:08:31 +0100

    Create a handler for 404 errors

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """Our (single) Flask webapp instance"""
-from flask import Flask
+from flask import Flask, jsonify
 from models import storage
 from api.v1.views import app_views
 from os import getenv
@@ -12,6 +12,15 @@ app.register_blueprint(app_views)
 @app.teardown_appcontext
 def teardown_db(exception):
     storage.close()
+
+
+@app.errorhandler(404)
+def handle_notfound(exception):
+    status = {
+        'error': 'Not found'
+    }
+
+    return jsonify(status), 404
 
 
 if __name__ == '__main__':
