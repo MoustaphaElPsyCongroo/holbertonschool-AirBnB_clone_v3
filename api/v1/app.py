d6ad1243a2aa80b131db1ff24352d3c58d26a142
MousElPsyCongroo <mtapha.tall@gmail.com>
2022-12-07 13:05:39 +0100

    13 - Add CORS handling

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,11 +1,13 @@
 #!/usr/bin/python3
 """Our (single) Flask webapp instance"""
 from flask import Flask, jsonify
+from flask_cors import CORS
 from models import storage
 from api.v1.views import app_views
 from os import getenv
 
 app = Flask(__name__)
+CORS(app, origins="0.0.0.0")
 app.url_map.strict_slashes = False
 app.register_blueprint(app_views)
 
